18a6f968a941f9c64167f70aeb48d8133a0674ca
btirumala <barath.tirumala@gmail.com>
2018-11-15 17:10:27 -0500

    Compile errors

--- MulticlassWeka.java
+++ MulticlassWeka.java
@@ -19,10 +19,10 @@ import weka.core.Instances;
  * Author:  Dr. Demetrios Glinos
  */
 public class MulticlassWeka {
-   private static final DecimalFormat DF = new DecimalFormat("0.00"); 
-   
+   private static final DecimalFormat DF = new DecimalFormat("0.00");
+
    public static BufferedReader findDataFile(String filename) {
-   
+
       BufferedReader inputReader = null;
       try {
          inputReader = new BufferedReader(new FileReader(filename));
@@ -31,22 +31,22 @@ public class MulticlassWeka {
       }
       return inputReader;
    }
- 
+
    public static Evaluation classify(
          Classifier model, Instances dataSet ) throws Exception {
-      
+
       Evaluation evaluation = new Evaluation(dataSet);
       model.buildClassifier(dataSet);
 
       evaluation.evaluateModel(model, dataSet);
       System.out.println( evaluation.toSummaryString("\nResults:\n", true) );
-      
+
       return evaluation;
    }
- 
+
    public static double calculateAccuracy(ArrayList<Prediction> predictions) {
       double correct = 0;
- 
+
       for (int i = 0; i < predictions.size(); i++) {
          NominalPrediction np = (NominalPrediction) predictions.get(i);
          if (np.predicted() == np.actual()) {
@@ -55,9 +55,9 @@ public class MulticlassWeka {
       }
       return 100 * correct / predictions.size();
    }
- 
+
    public static void main(String[] args) throws Exception {
-      
+
       // Load data file of instances
       BufferedReader datafile = findDataFile(args[0]);
       Instances data = new Instances(datafile);
@@ -68,7 +68,7 @@ public class MulticlassWeka {
       options[0] = args[0];   // input file name
       options[1] = args[1];   // number of training epochs
       Classifier model = new MulticlassPerceptron( options );
-                  
+
       // Run classifier and report results
       Evaluation validation = classify(model, data);
       ArrayList<Prediction> predictions = validation.predictions();
@@ -76,11 +76,10 @@ public class MulticlassWeka {
       // Final score
       double accuracy = calculateAccuracy(predictions);
       System.out.println("Accuracy of "
-            + model.getClass().getSimpleName() + ": " 
+            + model.getClass().getSimpleName() + ": "
             + DF.format(accuracy) + " %\n");
- 
+
       // Echo experiment setup and final weights
-      System.out.println(model.toString() + "\n\n"); 
+      System.out.println(model.toString() + "\n\n");
    }
 }
-         
